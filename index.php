961e67e36717382e0f95aa63e14fa0698a4e806b
Brenda Wallace <shiny@cpan.org>
2010-11-19 17:42:29 +1300

    reads xml files now

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+require('./governments.inc.php');
+do_command('rm -rf ./repo');
+mkdir('repo');
 
-$d = dir('./www.legislation.govt.nz/subscribe');
+
+do_command("git init");
+
 foreach(dirListing() as $doc_type) {
   echo "=== INDEXING  $doc_type \n";
   foreach(dirListing($doc_type) as $doc_area) {
@@ -11,6 +16,7 @@ foreach(dirListing() as $doc_type) {
         echo "\t\t$doc_type/$doc_area/$year/$did\n";
         foreach(dirListing("$doc_type/$doc_area/$year/$did") as $version) {
           echo "\t\t\t$doc_type/$doc_area/$year/$did/$version\n";
+          commit_document($doc_type, $doc_area, $year, $did, $version);
         }
       }
     }
@@ -19,8 +25,25 @@ foreach(dirListing() as $doc_type) {
 }
 
 
-function dirListing($dir='') {
-  $d = dir('./www.legislation.govt.nz/subscribe/'. $dir);
+function commit_document($doc_type, $doc_area, $year, $did, $version) {
+  $xml = file_get_contents("www.legislation.govt.nz/subscribe/$doc_type/$doc_area/$year/$did/$version");
+  $xml = (array)new SimpleXMLElement($xml);
+  $body = (array)$xml['body'];
+  $ul = (array)$body['ul'];
+  
+  $xml_file = (array)$ul['li'][0];
+  $pdf = (array)$ul['li'][1];
+  $xml_filename = $xml_file['a'] ."\n";
+  $pdf_filename = $pdf['a'];
+  $title = preg_replace('!\.pdf$!', '', $pdf_filename);
+  echo "title = $title\n";
+
+  
+//   var_dump((array)$xml);
+}
+
+function dirListing($dir='.') {
+  $d = dir("www.legislation.govt.nz/subscribe/$dir");
 
   while (false !== ($entry = $d->read())) {
     if ('.' != $entry && '..' != $entry) {
@@ -29,4 +52,57 @@ function dirListing($dir='') {
   }
   $d->close();
   return $list;
+}
+
+/*
+
+
+
+$legislation = get_legislation();
+foreach($legislation as $act) {
+  getNext($act['date'], $act['url'], $act['act']);
+}
+
+
+do_command('git filter-branch --commit-filter \'
+        GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE";
+        git commit-tree "$@";
+\' HEAD
+');*/
+
+/*
+function getNext($date, $url, $act) {
+  mkdir($act);
+
+  chdir($act);
+  print "$data $url\n";
+
+  do_command("wget $url");
+  do_command("mv *.pdf $act.pdf");
+  do_command("rm *.txt");
+  do_command("ls -la");
+  do_command("pdftotext *.pdf");
+  do_command("ls -la");
+  do_command("rm *.pdf");
+
+
+  do_command("git add $act*.txt");
+
+  $time = strtotime($date);
+  $year = date('Y', $time);
+  $government = government($year);
+  if(!$government) $government = 'Victoria';
+
+
+  chdir('..');
+  do_command("git add $act");
+  do_command("git commit -a --date=\"". $time .'" -m "'. $act .' Act at '. $date .'" --author="' . $government .  ' <nzparliamen@devnull.org>"');
+  do_command("git status");
+
+}*/
+
+function do_command($command) {
+  echo  getcwd()."\$ $command\n";
+  passthru($command);
+#  sleep(1);
 }
